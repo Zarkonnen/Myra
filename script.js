23ae55b3c34a0ff592b8824311d3031b25b949ff
Zarkonnen <david.stark@zarkonnen.com>
2023-12-09 12:39:51 +0100

    Intro, outro, pickups

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const roadT = {x: 64, y: 176};
 var scale = 1;
 var edit = false;
 const hurtTime = 80 * 3 + 20;
+const maxHP = 12;
+var level = 0;
+var intro = true;
+var victory = false;
 
 const nickWalk = {x:256, y:0, frames: 8, period: 80};
 const nickAttack = {x: 384, y: 0, frames: 6, period: 80};
@@ -37,7 +41,26 @@ var enemyType = {
     }
 };
 
+var pickupType = {
+    wine: {
+        x: 256,
+        y: 64,
+        hp: 4
+    },
+    bread: {
+        x: 272,
+        y: 64,
+        hp: 3
+    },
+    church: {
+        x: 288,
+        y: 64,
+        hp: 12
+    }
+};
+
 var enemies = [];
+var pickups = [];
 
 var player = null;
 
@@ -52,21 +75,31 @@ function reset() {
         attacking: false,
         landTime: 0,
         stun: 0,
-        hp: 10
+        hp: maxHP
     };
-    enemies = [
-        {
-            type: enemyType.pimp,
-            x: 200,
+    
+    enemies = levels[level].enemies.map(x => {
+        return {
+            type: enemyType[levels[level].enemyType],
+            x: x,
             y: 104,
             animTime: 0,
             flip: true,
             stun: 0,
-            hp: 2,
+            hp: enemyType[levels[level].enemyType].hp,
             attacking: false,
             cooldown: 0
         }
-    ];
+    });
+    pickups = levels[level].pickups.map(p => {
+        return {
+            x: p.x,
+            y: p.y,
+            type: pickupType[p.type]
+        }
+    });
+    intro = true;
+    victory = false;
 }
 
 reset();
@@ -89,7 +122,7 @@ function genTiles(w, h) {
     return ts;
 }
 
-tiles = level1;//genTiles(64, 8);
+tiles = levels[0].tiles;//genTiles(64, 8);
 
 function tick(ms) {
     t += ms;
@@ -99,12 +132,37 @@ function tick(ms) {
     }
     jQuery(canvas).css("width", (320 * scale)).css("height", (224 * scale));
     
-    c.fillStyle = "black";
+    c.fillStyle = "#00021c";
     c.fillRect(0, 0, 320, 224);
+    
+    if (victory && !edit) {
+        blit(levels[level].victory, (320 - 96) / 2, 16);
+        drawText("E OR SPACE TO CONTINUE", 16, 224 - 32, 1);
+        if (pressed(" ") || pressed("E")) {
+            //level++;
+            reset();
+        }
+        return;
+    }
+    
+    if (intro && !edit) {
+        drawText(levels[level].date, 16, 16, 2);
+        drawText(levels[level].intro, 16, 32, 0);
+        drawText("E OR SPACE TO START", 16, 224 - 32, 1);
+        if (pressed(" ") || pressed("E")) {
+            intro = false;
+        }
+        return;
+    }
     
     for (var y = 0; y < tiles.length; y++) { for (var x = 0; x < tiles[0].length; x++) {
         blit(tiles[y][x], x * 16 - scrollX, y * 16);
     }}
+    
+    // Pickups
+    pickups.forEach(p => {
+        blit(p.type, p.x - scrollX, p.y);
+    });
     
     if (edit) {
         blit(selection, 0, 0);
@@ -125,6 +183,10 @@ function tick(ms) {
             console.log(JSON.stringify(tiles));
         }
         
+        var tx = Math.floor((cursor.x + scrollX) / 16);
+        var ty = Math.floor(cursor.y / 16);
+        drawText(tx + ", " + ty, 320 - 48, 0);
+        
         if (palette) {
             blit({x: 0, y: 128, w: 256, h: 192}, 0, 0);
             if (click) {
@@ -136,8 +198,6 @@ function tick(ms) {
             c.fillRect(selection.x + 15, selection.y - 128, 1, 16);
             c.fillRect(selection.x, selection.y - 128 + 15, 16, 1);
         } else if (mouseDown) {
-            var tx = Math.floor((cursor.x + scrollX) / 16);
-            var ty = Math.floor(cursor.y / 16);
             if (tx >= 0 && tx < tiles[0].length && ty < tiles.length) {
                 tiles[ty][tx] = selection;
             }
@@ -207,9 +267,22 @@ function tick(ms) {
         player.landTime -= ms;
     }
     
+    if (player.x >= tiles[0].length * 16 - 6 * 16) {
+        victory = true;
+    }
+    
     player.x = Math.max(0, Math.min(tiles[0].length * 16 - 16, player.x));
     var prevScrollX = scrollX;
     scrollX = Math.min(tiles[0].length * 16 - 320, Math.max(0, Math.floor(player.x + 8 - 160)));
+    
+    for (var i = 0; i < pickups.length; i++) {
+        var p = pickups[i];
+        if (player.hp < maxHP && p.x + 4 <= player.x + 12 && p.x + 12 >= player.x + 4 && p.y <= player.y + 16 && p.y + 16 >= player.y) {
+            player.hp = Math.min(maxHP, player.hp + p.type.hp);
+            pickups.splice(i, 1);
+            i--;
+        }
+    }
     
     if (player.hp <= 0) {
         player.animTime += ms;
@@ -263,6 +336,7 @@ function tick(ms) {
         blit(nickWalk, Math.floor(player.x) - scrollX, Math.floor(player.y), player.animTime, player.flip);
     }
     
+    // Balconies
     for (var y = 0; y < tiles.length; y++) { for (var x = 0; x < tiles[0].length; x++) {
         if (tiles[y][x].x == 224 && tiles[y][x].y == 192) {
             blit({x: 224, y: 176}, x * 16 - prevScrollX, y * 16 - 16);
@@ -312,6 +386,14 @@ function tick(ms) {
     
     enemies = enemies.filter(e => e.hp > 0 || e.animTime < e.type.deathAnim.frames * e.type.deathAnim.period);
     
+    c.fillStyle = "#a3ccd9";
+    c.fillRect(16, 144, 80, 16);
+    c.fillStyle = "#1c284d";
+    c.fillRect(17, 145, 78, 14);
+    c.fillStyle = "#d9214f";
+    c.fillRect(18, 146, Math.floor(76.0 * Math.max(0, player.hp) / maxHP), 12);
+    drawText("HP", 19, 144);
+    
     if (player.hp <= 0) {
         c.fillStyle = "black";
         if (player.animTime > 4500) {
@@ -355,6 +437,159 @@ function blit(img, x, y, time, flipped) {
         c.drawImage(images["spritesheet"], img.x, img.y, img.w || 16, img.h || 16, 0, 0, img.w || 16, img.h || 16);
     }
     c.resetTransform();
+}
+
+var offsets = {
+    A: 0,
+    B: 9,
+    C: 17,
+    D: 24,
+    E: 32,
+    F: 40,
+    G: 48,
+    H: 55,
+    I: 64,
+    J: 69,
+    K: 76,
+    L: 85,
+    M: 93,
+    N: 103,
+    O: 113,
+    P: 121,
+    Q: 129,
+    R: 137,
+    S: 146,
+    T: 153,
+    U: 162,
+    V: 170,
+    W: 177,
+    X: 188,
+    Y: 195,
+    Z: 204,
+    "?": 477,
+    "!": 482,
+    ".": 463,
+    ",": 466,
+    ";": 470,
+    ":": 474,
+    "'": 521,
+    "0": 393,
+    "1": 400,
+    "2": 407,
+    "3": 414,
+    "4": 421,
+    "5": 428,
+    "6": 435,
+    "7": 442,
+    "8": 449,
+    "9": 456,
+    "(": 532,
+    ")": 537,
+    "-": 485
+};
+
+var widths = {
+    A: 9,
+    B: 8,
+    C: 7,
+    D: 8,
+    E: 8,
+    F: 8,
+    G: 7,
+    H: 9,
+    I: 5,
+    J: 7,
+    K: 9,
+    L: 8,
+    M: 10,
+    N: 10,
+    O: 8,
+    P: 8,
+    Q: 8,
+    R: 8,
+    S: 7,
+    T: 9,
+    U: 8,
+    V: 7,
+    W: 11,
+    X: 7,
+    Y: 9,
+    Z: 7,
+    "?": 5,
+    "!": 3,
+    ".": 3,
+    ",": 4,
+    ";": 4,
+    ":": 4,
+    "'": 4,
+    "0": 7,
+    "1": 7,
+    "2": 7,
+    "3": 7,
+    "4": 7,
+    "5": 7,
+    "6": 7,
+    "7": 7,
+    "8": 7,
+    "9": 7,
+    "(": 5,
+    ")": 5,
+    "-": 7
+}
+
+function textWidth(s) {
+    var w = 0;
+    for (var i = 0; i < s.length; i++) {
+        w += widths[s[i]] || 7;
+    }
+    return w;
+}
+
+function drawText(s, x, y, color) {
+    if (!images["font-black"]) {
+        images["font-black"] = new Image();
+        images["font-black"].src = "graphics/font-black.png" + "?" + (new Date()).getTime();
+        images["font-white"] = new Image();
+        images["font-white"].src = "graphics/font-white.png" + "?" + (new Date()).getTime();
+        images["font-grey"] = new Image();
+        images["font-grey"].src = "graphics/font-grey.png" + "?" + (new Date()).getTime();
+        images["font-yellow"] = new Image();
+        images["font-yellow"].src = "graphics/font-yellow.png" + "?" + (new Date()).getTime();
+    }
+    color = ["white", "grey", "yellow"][color || 0];
+    var x2 = x;
+    var y2 = y;
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] == "\n") {
+            y2 += 16;
+            x2 = x;
+            continue;
+        }
+        if (!widths[s[i]]) {
+            x2 += 7;
+            continue;
+        }
+        for (var dy = 0; dy <= 2; dy++) { for (var dx = 0; dx <= 2; dx++) {
+            c.drawImage(images["font-black"], offsets[s[i]], 0, widths[s[i]], 16, x2 + dx, y2 + dy, widths[s[i]], 16);
+        }}
+        x2 += widths[s[i]];
+    }
+    x2 = x;
+    y2 = y;
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] == "\n") {
+            y2 += 16;
+            x2 = x;
+            continue;
+        }
+        if (!widths[s[i]]) {
+            x2 += 7;
+            continue;
+        }
+        c.drawImage(images["font-" + color], offsets[s[i]], 0, widths[s[i]], 16, x2 + 1, y2 + 1, widths[s[i]], 16);
+        x2 += widths[s[i]];
+    }
+    return y2 + 16;
 }
 
 var canvas = document.getElementById("gameCanvas");
